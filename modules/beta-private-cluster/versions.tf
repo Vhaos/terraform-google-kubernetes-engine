55da0bd5a55c697dea335ffb0a85bba2dc1610f5
Kareem Daggash <kareemdaggash@gmail.com>
2024-10-04 15:39:05 +0100

    oops was changing wrong versions file

--- modules/beta-private-cluster/versions.tf
+++ modules/beta-private-cluster/versions.tf
@@ -21,11 +21,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.0.0, < 6"
+      version = ">= 6.0.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.0.0, < 6"
+      version = ">= 6.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
